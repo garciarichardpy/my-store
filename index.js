f99a373ba2d7a0f7c275dff528615e9a644c1c3d
richard96py <richard@R-GARCIA-RASP.localdomain>
2023-11-29 17:40:03 -0300

    proyecto

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require('express')
+const routerApi = require('./routes')
+const cors = require('cors')
+
+const {logErrors, errorHandler, boomErrorHandler} = require('./middlewares/error.handler')
+
+
+const app = express()
+const port = 3000;
+
+app.use(express.json())
+
+const whiteList = ['http://localhost:8080', 'https://myapp.com.py']
+const options = {
+    origin: (origin, callback) => {
+        if(whiteList.includes(origin)){
+            callback(null, true)
+        }else{
+            callback(new Error('No permitido'))
+        }
+    }
+}
+
+
+app.use(cors())
+
+app.get('/', (req, res) => {
+    res.send('Hola mi server en express')
+})
+
+app.get('/nueva-ruta', (req, res)=> {
+    res.send('Hola soy una nueva ruta')
+})
+
+
+routerApi(app)
+
+app.use(logErrors)
+app.use(boomErrorHandler)
+app.use(errorHandler)
+
+
+app.listen(port, () => {
+    console.log('Mi port' + port)
+})
+
+
+
